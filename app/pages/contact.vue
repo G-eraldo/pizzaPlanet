73249f8fef9467479c3a2bafe628cc838b5da1bb
G_eraldo <gerald-f@hotmail.fr>
2025-11-29 13:57:35 +0100

    ajout condition textarea

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -84,7 +84,7 @@ async function submitForm() {
                             <Label html-for="message" class="block text-sm font-medium text-slate-700 mb-1">
                                 Ecrivez votre message
                             </Label>
-                            <textarea v-model="textarea" name="message" rows="4" required class="w-full px-4 py-2 border
+                            <textarea v-model="textarea" name="message" rows="4" required minlength="10" class="w-full px-4 py-2 border
                                 border-slate-300 rounded-lg focus:ring-amber-500 focus:border-amber-500 transition
                                 duration-150 " placeholder="Posez votre question ici" />
 
